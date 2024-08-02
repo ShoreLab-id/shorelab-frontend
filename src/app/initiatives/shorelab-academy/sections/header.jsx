d5c5865742ed922047bf45ba21255480f8e6effd
PetrusAriaa <petrusariacr25@gmail.com>
2024-08-02 20:40:50 +0700

    chore: code tidy

--- src/app/initiatives/shorelab-academy/sections/header.jsx
+++ src/app/initiatives/shorelab-academy/sections/header.jsx
@@ -2,24 +2,11 @@ import { poppins, oxygen } from "@/templates/font";
 
 const HeaderSection = () => {
   return (
-    <div className="w-full h-screen">
-      <div className="w-full h-[75%] bg-primary-dark flex justify-center">
-        <div className="w-[90%] max-w-[1300px] h-[65%] flex flex-col justify-end">
-          <h1
-            className={
-              poppins + "text-[84px] font-semibold text-secondary-white"
-            }
-          >
-            ShoreLab Academy
-          </h1>
-          <p
-            className={
-              oxygen +
-              "text-[27px] font-semibold text-secondary-white max-w-[530px]"
-            }
-          >
-            Leading Marine Conservation Through Tech Innovation
-          </p>
+    <div className="w-full pb-20">
+      <div className="w-full h-96 bg-primary-dark flex flex-col justify-end items-center">
+        <div className="w-[90%] max-w-[1300px] flex flex-col">
+          <h1 className={poppins + "text-[84px] font-semibold text-secondary-white"}>ShoreLab Academy</h1>
+          <p className={oxygen + "text-[27px] font-semibold text-secondary-white max-w-[530px]"}>Leading Marine Conservation Through Tech Innovation</p>
         </div>
         <div className="relative w-[90%] max-w-[1080px] h-32 bg-white drop-shadow-md shadow-lg rounded-md -bottom-14"/>
       </div>
