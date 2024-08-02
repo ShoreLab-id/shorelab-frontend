d1e1fa7d44d915d0226f2e9cf7be67f483921626
PetrusAriaa <petrusariacr25@gmail.com>
2024-08-02 18:06:23 +0700

    feat: add Navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import MyNavbar from "@/templates/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children} </Providers>
+        <Providers>
+          <MyNavbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
