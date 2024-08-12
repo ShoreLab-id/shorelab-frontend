544bdb09be9cd055e59917be5890a4e0cb2c3c76
RazzaTitian <zeusprototype01@gmail.com>
2024-08-12 13:41:28 +0700

    feat: new page background

--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -5,7 +5,7 @@ import Content from "./section/content";
 
 const TermsPage = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-primary-dark flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl w-full space-y-8">
         <Content />
       </div>
