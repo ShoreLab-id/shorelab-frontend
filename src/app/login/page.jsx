e9ffb2ce6df776a2d9eea7070757d9cf1a493a13
RazzaTitian <zeusprototype01@gmail.com>
2024-08-14 16:22:19 +0700

    fix: replaced img with Image from NextUI

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,21 +2,20 @@
 
 import React from "react";
 import LoginForm from "./section/form";
+import { Image } from "@nextui-org/react";
 
 const LoginPage = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center bg-black">
       <div className="flex">
-        {/* Left Section with Image */}
         <div className="w-full h-full">
-          <img
+          <Image
             src="/static/Juan.jpg"
             alt="Juan"
-            className="object-cover bg-red-500/25"
-          />
+            className="object-cover
+            bg-red-500/25"
+          ></Image>
         </div>
-
-        {/* Right Section with Form */}
         <div className="w-1/2 h-full flex items-center justify-center bg-red-500/25">
           <LoginForm />
         </div>
