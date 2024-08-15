e3e45ec6e5e5127235a0627e902746f37c86f9f4
PetrusAriaa <petrusariacr25@gmail.com>
2024-08-15 23:00:03 +0700

    style: add images and parallax effects

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,18 +1,31 @@
 "use client";
 
-import Image  from "next/image";
-import { IoMdArrowDown } from "react-icons/io";
-import { useEffect } from "react";
+import SectionHeading from "@/templates/heading";
+import Image from "next/image";
+import { poppins, oxygen } from "@/templates/font";
 
 const AboutPage = () => {
   window.addEventListener("scroll", (e) => {
-    const aboutSection = document.getElementById("about")
+    let aboutSection = document.getElementById("about")
     const title = document.getElementById("title");
+    const contents = document.querySelectorAll(".__about-content__")
     const axY = window.scrollY
-    aboutSection.style.transform = 'translateY(' + (-192 + (0.6 * -axY)) + 'px)';
+    // console.log(axY > section5.getBoundingClientRect().top)
+    // console.log(section5.getBoundingClientRect().top - section5.getBoundingClientRect().bottom)
+    aboutSection.style.transform = 'translateY(' + (-80 + (0.6 * -axY)) + 'px)';
     title.style.transform = 'translateY(' + (0.75 * axY) + 'px)';
     title.style.scale = (100 - (0.05 * axY))/100;
     title.style.opacity = (100 - (0.5 * axY))/100;
+    title.style.filter = 'blur('+ 0.1 * axY + 'px)'
+    contents.forEach((content) => {
+      if (axY > content.getBoundingClientRect().bottom) {
+        const _disp = axY - content.getBoundingClientRect().bottom
+        console.log(axY, window.innerHeight - content.getBoundingClientRect().bottom)
+        if (0.05 * _disp < 100) {
+          content.style.transform = 'translateY(' + (-0.05 * _disp) + 'px)';
+        }
+      }
+    })
   })
 
   return (
@@ -23,63 +36,84 @@ const AboutPage = () => {
           <div className="w-[80%] max-w-[1300px] flex flex-col justify-center items-center gap-20">
             <div id="title" className="flex gap-10 items-center justify-center">
               <div className="size-32 bg-slate-200 animate-pulse rounded-lg" />
-              <h1 className="text-7xl font-bold text-white">ShoreLab</h1>
+              <h1 className={oxygen + "text-9xl font-bold text-white"}>ShoreLab</h1>
             </div>
-            {/* <IoMdArrowDown className="size-16 bg-primary-white/20 text-white p-2 rounded-full animate-bounce" /> */}
           </div>
         </div>
       </section>
 
-      <section className="relative flex justify-center w-full">
+      <section className="relative flex justify-center w-full bg-red-500/25 h-fit max-h-[1600px]">
         <div
           id="about"
-          className="bg-primary-dark w-[80%] max-w-[1300px] py-20 text-white z-[1] rounded-2xl drop-shadow-md shadow-xl -translate-y-48"
+          className="bg-primary-dark w-[80%] max-w-[1300px] text-white z-[1] rounded-2xl drop-shadow-md shadow-xl -translate-y-20 p-20 flex flex-col gap-32 h-fit"
         >
-          <div className="relative z-10 max-w-[1300px] mx-auto px-6">
-            <div className="flex flex-col md:flex-row items-center md:items-start mb-20">
-              <div className="w-full md:w-1/2 lg:w-[720px] mb-6 md:mb-0">
-                <img
-                  src="https://via.placeholder.com/720x400"
-                  alt="Placeholder"
-                  className="w-full h-auto rounded-tl-xl rounded-bl-xl"
-                />
-              </div>
-              <div className="flex flex-col gap-6 p-6 md:p-12 lg:p-14 justify-center">
-                <h1 className="text-[27px] font-semibold leading-snug text-[#00A8A8]">
-                  About Us
-                </h1>
-                <h2 className="font-semibold text-[48px]">Who are we?</h2>
-                <p className="text-lg max-w-[630px]">
-                  We are a team of three dedicated members from different
-                  universities, united by a single mission: to conserve the
-                  vibrant coral reefs of Indonesia. Our shared passion for
-                  marine preservation drives us to collaborate and innovate,
-                  ensuring a sustainable future for our beloved country&apos;s
-                  underwater ecosystems.
-                </p>
-              </div>
+          <div className="flex flex-row gap-10">
+            <div className="relative w-3/5 aspect-[5/4] rounded-2xl shadow-xl drop-shadow-md">
+              <Image src="/static/about_1.jpg" alt="" fill className="object-cover rounded-2xl" sizes="50vw" />
             </div>
+            <div className="w-2/5 __about-content__">
+              <div about="section-heading" className="mb-12">
+                <h2 className={poppins + "text-[27px] font-semibold capitalize text-teal-secondary"}>THE AWAKENING</h2>
+                <h1 className={poppins + "text-[48px] font-semibold text-white leading-tight"}>A Passion for the Ocean</h1>
+              </div>
+              <p className="text-pretty" >Our journey started with a love for the ocean, where we found peace and inspiration. But as we explored further, we began to see the signs of a struggling marine ecosystem—bleached corals, polluted waters, and declining marine life.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row-reverse gap-10">
+            <div className="relative w-2/5 aspect-[4/5] rounded-2xl shadow-xl drop-shadow-md">
+              <Image src="/static/about_2.png" alt="" fill className="object-cover rounded-2xl" sizes="50vw" />
+              {/* https://www.ipb.ac.id/wp-content/uploads/2024/07/Sea-Farming-IPB-University-Bersama-Dinas-Ketahanan-Pangan-Kelautan-dan-Pertanian-DKI-Jakarta-Mengawal-Pembangunan-Budaya-Maritim.png */}
+            </div>
+            <div className="w-2/5 __about-content__">
+              <div about="section-heading" className="mb-12">
+                <h2 className={poppins + "text-[27px] font-semibold capitalize text-teal-secondary"}>THE REALIZATION</h2>
+                <h1 className={poppins + "text-[48px] font-semibold text-white leading-tight"}>A Call to Action</h1>
+              </div>
+              <p className="text-pretty" >Seeing the ocean&apos;s distress sparked a deep concern within us. We realized that our passion for marine life came with a responsibility to protect it. This realization became the driving force behind our mission.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-10">
+            <div className="relative w-2/5 aspect-square rounded-2xl shadow-xl drop-shadow-md">
+              <Image src="/static/about_3.png" alt="" fill className="object-cover rounded-2xl" sizes="50vw" />
+            </div>
+            <div className="w-2/5 __about-content__">
+              <div about="section-heading" className="mb-12">
+                <h2 className={poppins + "text-[27px] font-semibold capitalize text-teal-secondary"}>THE VISION</h2>
+                <h1 className={poppins + "text-[48px] font-semibold text-white leading-tight"}>Founding ShoreLab</h1>
+              </div>
+              <p className="text-pretty" >ShoreLab was established with a clear purpose: to create a platform where knowledge meets action. We aimed to equip students with the tools and experiences needed to address the pressing challenges facing our oceans.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row-reverse gap-10">
+            <div className="relative w-3/5 aspect-[5/4] rounded-2xl shadow-xl drop-shadow-md">
+              <Image src="/static/about_4.png" alt="" fill className="object-cover rounded-2xl" sizes="50vw" />
+            </div>
+            <div className="w-2/5 __about-content__">
+              <div about="section-heading" className="mb-12">
+                <h2 className={poppins + "text-[27px] font-semibold capitalize text-teal-secondary"}>THE JOURNEY</h2>
+                <h1 className={poppins + "text-[48px] font-semibold text-white leading-tight"}>Growing Together</h1>
+              </div>
+              <p className="text-pretty" >What began as a modest effort quickly grew into a collaborative movement. Through ShoreLab Academy, students from around the world now come together to learn, innovate, and make a lasting impact on marine conservation.</p>
+            </div>
+          </div>
+
+        <div className="flex flex-row gap-10">
+          {/* https://i.pinimg.com/originals/51/35/99/513599a7b70bbcc1ddb68765e534cd1e.jpg */}
+          <div className="relative w-2/5 aspect-[5/4] rounded-2xl shadow-xl drop-shadow-md">
+              <Image src="/static/about_5.jpg" alt="" fill className="object-cover rounded-2xl" sizes="50vw" />
+            </div>
+          <div id="section5" className="w-3/5 __about-content__ bg-red-500/25">
+            <div about="section-heading" className="mb-12">
+              <h2 className={poppins + "text-[27px] font-semibold capitalize text-teal-secondary"}>THE IMPACT</h2>
+              <h1 className={poppins + "text-[48px] font-semibold text-white leading-tight"}>Creating Waves of Change</h1>
+            </div>
+            <p className="text-pretty" >Today, ShoreLab is a beacon of hope for the ocean. Our projects have restored habitats, protected marine life, and inspired thousands to join the cause. Every action we take creates ripples, leading to a healthier and more sustainable ocean.</p>
           </div>
         </div>
-
-        {/* <section
-          id="projects-section"
-          className="relative w-full py-20 text-white"
-        >
-          <div className="relative z-10 max-w-[1300px] mx-auto px-6">
-            <h2 className="text-[24px] text-[#00A8A8] font-semibold">
-              Projects
-            </h2>
-            <h3 className="text-[36px] font-semibold mb-8">
-              What have we done?
-            </h3>
-            <div className="flex justify-center items-center gap-6">
-              <div className="w-[200px] h-[300px] bg-gray-300 rounded-lg"></div>
-              <div className="w-[300px] h-[400px] bg-gray-300 rounded-lg"></div>
-              <div className="w-[200px] h-[300px] bg-gray-300 rounded-lg"></div>
-            </div>
-          </div>
-        </section> */}
+        </div>
       </section>
 
       <section className="relative w-full py-20 bg-white text-black">
