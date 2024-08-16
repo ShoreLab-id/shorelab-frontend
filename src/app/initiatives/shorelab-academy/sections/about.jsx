c40c4b222064d11e0158b7cc35afd8ecde5e0488
PetrusAriaa <petrusariacr25@gmail.com>
2024-08-17 02:52:03 +0700

    refactor: reuse Section Heading component

--- src/app/initiatives/shorelab-academy/sections/about.jsx
+++ src/app/initiatives/shorelab-academy/sections/about.jsx
@@ -1,35 +1,19 @@
-import { poppins, oxygen } from "@/templates/font";
+import { oxygen } from "@/templates/font";
+import SectionHeading from "@/templates/heading";
 
 const AboutSection = () => {
   return (
-    <div className="w-full flex justify-center py-20 bg-red-500/25">
-      <div className="w-[90%] max-w-[1300px] h-full bg-blue-500/25">
+    <div className="w-full flex justify-center pt-32 pb-20 bg-[#F3F9FC]">
+      <div className="w-[80%] max-w-[1300px] h-full">
         <div className="flex gap-[130px] items-center">
           <div className="w-[530px] h-[525px] bg-slate-200 animate-pulse rounded-xl flex-shrink-0" />
           <div>
-            <div about="section-heading" className="mb-12 bg-red-500/25">
-              <h2
-                className={
-                  poppins +
-                  "text-[27px] font-semibold capitalize text-teal-secondary"
-                }
-              >
-                ABOUT US
-              </h2>
-              <h1
-                className={
-                  poppins +
-                  "text-[48px] font-semibold text-primary-dark leading-tight"
-                }
-              >
-                What is ShoreLab Academy?
-              </h1>
-            </div>
+            <SectionHeading sub="ABOUT US" title="What is ShoreLab Academy?" />
             <p
               className={oxygen + "text-primary-dark text-justify mb-4 text-xl"}
             >
               For the past two years, ShoreLab Academy has empowered university
-              students with hands-on marine conservation experience. We’ve
+              students with hands-on marine conservation experience. We&apos;ve
               completed 8 projects involving 167 participants, collaborating
               with local marine organizations, the Ministry of Maritime Affairs
               and Fishery, and Kampus Merdeka.
