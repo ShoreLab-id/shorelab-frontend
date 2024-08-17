40195cb6df0535e4e3c6c03e8ca5e5b855955986
PetrusAriaa <petrusariacr25@gmail.com>
2024-08-18 03:04:36 +0700

    feat: add remote image server

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shorelab-backend.vercel.app',
+        port: '',
+        pathname: '/api/static/**'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
