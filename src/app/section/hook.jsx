2d364546320b437bf0fd3ee5cf182169acf4e4c4
RazzaTitian <zeusprototype01@gmail.com>
2024-08-17 16:43:31 +0700

    fix: changed svg to webp. Yes, I forgot.

--- src/app/section/hook.jsx
+++ src/app/section/hook.jsx
@@ -3,25 +3,44 @@ import SectionHeading from "@/templates/heading";
 
 const UserHook = () => {
   return (
-    <main className="bg-teal-secondary bg-[url('/assets/topography.svg')] w-full flex justify-center py-20" >
+    <main className="bg-teal-secondary bg-[url('/assets/topography.webp')] w-full flex justify-center py-20">
       <div className="w-[80%] max-w-[1300px] rounded-xl flex flex-col items-center justify-center">
-        <h1 className={oxygen + "text-[36px] text-white text-center text-pretty"}>Impact waves across <b>marine conservation and innovation</b> in numbers</h1>
+        <h1
+          className={oxygen + "text-[36px] text-white text-center text-pretty"}
+        >
+          Impact waves across <b>marine conservation and innovation</b> in
+          numbers
+        </h1>
         <div className={oxygen + "grid grid-cols-4 pt-8 gap-16"}>
           <div className="text-center text-white max-w-56 flex flex-col gap-4">
-            <p className="text-[48px] leading-none"><b>23</b></p>
+            <p className="text-[48px] leading-none">
+              <b>23</b>
+            </p>
             <p className="text-[21px] leading-snug">projects have concluded</p>
           </div>
           <div className="text-center text-white max-w-56 flex flex-col gap-4">
-            <p className="text-[48px] leading-none"><b>290+</b></p>
+            <p className="text-[48px] leading-none">
+              <b>290+</b>
+            </p>
             <p className="text-[21px] leading-snug">people have participated</p>
           </div>
           <div className="text-center text-white max-w-56 flex flex-col gap-4">
-            <p className="text-[48px] leading-none"><b>43.22+ </b><span className="text-[27px]">Ha</span></p>
-            <p className="text-[21px] leading-snug">of coral have been planted</p>
+            <p className="text-[48px] leading-none">
+              <b>43.22+ </b>
+              <span className="text-[27px]">Ha</span>
+            </p>
+            <p className="text-[21px] leading-snug">
+              of coral have been planted
+            </p>
           </div>
           <div className="text-center text-white max-w-56 flex flex-col gap-4">
-            <p className="text-[48px] leading-none"><b>100.2+ </b><span className="text-[27px]">Kg</span></p>
-            <p className="text-[21px] leading-snug">of plastic have been picked up</p>
+            <p className="text-[48px] leading-none">
+              <b>100.2+ </b>
+              <span className="text-[27px]">Kg</span>
+            </p>
+            <p className="text-[21px] leading-snug">
+              of plastic have been picked up
+            </p>
           </div>
         </div>
       </div>
