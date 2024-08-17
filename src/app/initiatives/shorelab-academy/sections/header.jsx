4cc4a1f100db2cb8378b70c3ecfa078e8379c5d6
RazzaTitian <zeusprototype01@gmail.com>
2024-08-17 17:07:39 +0700

    fix: temporary idea for page layout

--- src/app/initiatives/shorelab-academy/sections/header.jsx
+++ src/app/initiatives/shorelab-academy/sections/header.jsx
@@ -3,26 +3,44 @@ import { poppins, oxygen } from "@/templates/font";
 
 const HeaderSection = () => {
   return (
-      <div className="w-full h-[90vh] max-h-[1080px] bg-[url('/static/shorelab-hero.webp')] bg-cover bg-center">
-        <div className="w-full h-full bg-[#03202C]/85 flex flex-col justify-end items-center">
-            <div className="w-[70%] max-w-[1300px] flex items-center">
-              <div className="flex flex-col gap-8">
-                <h1 className={poppins + "text-[84px] font-semibold text-secondary-white leading-tight"}>ShoreLab Academy</h1>
-                <p className={oxygen + "text-[27px] font-semibold text-secondary-white max-w-[530px]"}>Leading Marine Conservation Through Tech Innovation</p>
-                <ButtonPrimary>Upcoming Project</ButtonPrimary>
-              </div>
-              <div className="p-2 bg-white rounded-2xl flex-shrink-0">
-                <div className="size-[30rem] overflow-hidden rounded-xl">
-                  <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d902872.4303051748!2d117.92678211944552!3d1.9796336607511547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x320db9032df93133%3A0x2cea5b974756a685!2sKabupaten%20Berau%2C%20Kalimantan%20Timur!5e0!3m2!1sid!2sid!4v1723833757343!5m2!1sid!2sid" 
-                  className="size-[30rem]" 
-                  loading="lazy"
-                  referrerpolicy="no-referrer-when-downgrade" />
-                </div>
-              </div>
+    <div className="w-full h-[90vh] max-h-[1080px] bg-[url('/static/shorelab-hero.webp')] bg-cover bg-center">
+      <div className="w-full h-full bg-[#03202C]/85 flex flex-col justify-end items-center">
+        <div className="w-[70%] max-w-[1300px] flex items-center">
+          <div className="flex flex-col gap-8">
+            <h1
+              className={
+                poppins +
+                "text-[84px] font-semibold text-secondary-white leading-tight"
+              }
+            >
+              ShoreLab Academy
+            </h1>
+            <p
+              className={
+                oxygen +
+                "text-[27px] font-semibold text-secondary-white max-w-[530px]"
+              }
+            >
+              Leading Marine Conservation Through Tech Innovation
+            </p>
+            <ButtonPrimary>Upcoming Project</ButtonPrimary>
+          </div>
+          <div className="p-2 bg-white rounded-2xl flex-shrink-0">
+            <div className="size-[30rem] overflow-hidden rounded-xl">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d902872.4303051748!2d117.92678211944552!3d1.9796336607511547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x320db9032df93133%3A0x2cea5b974756a685!2sKabupaten%20Berau%2C%20Kalimantan%20Timur!5e0!3m2!1sid!2sid!4v1723833757343!5m2!1sid!2sid"
+                className="size-[30rem]"
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              />
             </div>
-            <div data-aos="fade-up" className="relative w-[90%] max-w-[1080px] h-32 bg-white drop-shadow-md shadow-lg rounded-md -bottom-14"/>
-          {/* <div className="h-full w-full overflow-hidden relative">
+          </div>
+        </div>
+        <div
+          data-aos="fade-up"
+          className="relative w-[90%] max-w-[1080px] h-32 bg-teal-secondary drop-shadow-md shadow-lg rounded-md -bottom-14"
+        />
+        {/* <div className="h-full w-full overflow-hidden relative">
             <div className="absolute w-full -bottom-20">
               <img src="/assets/layer0_cloud.png" alt="" className="w-full" />
             </div>
@@ -42,8 +60,8 @@ const HeaderSection = () => {
               <img src="/assets/layer5.png" alt="" className="w-full" />
             </div>
           </div> */}
-        </div>
       </div>
+    </div>
   );
 };
 
--- src/app/initiatives/shorelab-academy/sections/thisYear.jsx
+++ src/app/initiatives/shorelab-academy/sections/thisYear.jsx
@@ -1,24 +1,45 @@
-import { poppins, oxygen } from "@/templates/font"
+import { poppins, oxygen } from "@/templates/font";
 
 const ThisYearSection = () => {
   return (
-    <div className="w-full flex justify-center py-20">
+    <div className="w-full flex justify-center py-20 pt-40 bg-primary-dark">
       <div className="w-[90%] max-w-[1300px] h-full">
         <div className="flex gap-[130px] items-center">
           <div>
             <div about="section-heading" className="mb-12">
-              <h2 className={poppins + "text-[27px] font-semibold capitalize text-teal-secondary"}>SAILING THIS YEAR</h2>
-              <h1 className={poppins + "text-[48px] font-semibold text-primary-dark leading-tight"}>Where the Waves Call</h1>
+              <h2
+                className={
+                  poppins +
+                  "text-[27px] font-semibold capitalize text-teal-secondary"
+                }
+              >
+                SAILING THIS YEAR
+              </h2>
+              <h1
+                className={
+                  poppins + "text-[48px] font-semibold text-white leading-tight"
+                }
+              >
+                Where the Waves Call
+              </h1>
             </div>
-            <p className={oxygen + "text-primary-dark text-justify mb-4 text-xl"}>
-              Embark on an exciting sea exploration with ShoreLab Academy as we venture into the pristine waters of Kabupaten Berau, East Kalimantan. Our latest initiative focuses on Sea Turtle and Jellyfish Conservation, aiming to develop innovative, technology-based solutions to address the challenges these majestic creatures face. From advanced tracking systems for sea turtles to sustainable methods for managing jellyfish populations, join us in harnessing the power of technology to protect and preserve this unique marine ecosystem.
+            <p className={oxygen + "text-white text-justify mb-4 text-xl"}>
+              Embark on an exciting sea exploration with ShoreLab Academy as we
+              venture into the pristine waters of Kabupaten Berau, East
+              Kalimantan. Our latest initiative focuses on Sea Turtle and
+              Jellyfish Conservation, aiming to develop innovative,
+              technology-based solutions to address the challenges these
+              majestic creatures face. From advanced tracking systems for sea
+              turtles to sustainable methods for managing jellyfish populations,
+              join us in harnessing the power of technology to protect and
+              preserve this unique marine ecosystem.
             </p>
           </div>
-          <div className="w-[640px] h-[464px] bg-slate-200 animate-pulse rounded-xl flex-shrink-0"/>
+          <div className="w-[640px] h-[464px] bg-slate-200 animate-pulse rounded-xl flex-shrink-0" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ThisYearSection
+export default ThisYearSection;
