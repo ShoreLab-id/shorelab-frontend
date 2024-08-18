d0185c22dcfdf137ca37d658e380cb4b2dac9819
PetrusAriaa <petrusariacr25@gmail.com>
2024-08-18 20:57:21 +0700

    fix: handle element id `title` not found

--- src/app/about/sections/title.jsx
+++ src/app/about/sections/title.jsx
@@ -6,11 +6,12 @@ const TitleSection = () => {
   window.addEventListener('scroll', () => {
     const title = document.getElementById("title");
     const axY = window.scrollY;
-
-    title.style.transform = "translateY(" + 0.75 * axY + "px)";
-    title.style.scale = (100 - 0.05 * axY) / 100;
-    title.style.opacity = (100 - 0.5 * axY) / 100;
-    title.style.filter = "blur(" + 0.1 * axY + "px)";
+    if (title){
+      title.style.transform = "translateY(" + 0.75 * axY + "px)";
+      title.style.scale = (100 - 0.05 * axY) / 100;
+      title.style.opacity = (100 - 0.5 * axY) / 100;
+      title.style.filter = "blur(" + 0.1 * axY + "px)";
+    }
   })
   return (
     <div
