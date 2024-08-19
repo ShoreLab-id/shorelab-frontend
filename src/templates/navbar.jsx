54d32bf3bd1bb6d9d9274a3faa141689b076fc3b
PetrusAriaa <petrusariacr25@gmail.com>
2024-08-19 14:15:00 +0700

    feat: add menu for mobile

--- src/templates/navbar.jsx
+++ src/templates/navbar.jsx
@@ -5,6 +5,9 @@ import {
   NavbarContent,
   NavbarItem,
   NavbarBrand,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
   Dropdown,
   DropdownTrigger,
   DropdownMenu,
@@ -15,6 +18,7 @@ import Link from "next/link";
 import { ButtonPrimary } from "./buttons";
 import { usePathname } from "next/navigation";
 import { FiChevronDown } from "react-icons/fi";
+import { BsList, BsXLg } from "react-icons/bs";
 import { useEffect, useState } from "react"
 
 const LoginButton = ({ onScroll, children }) => {
@@ -42,7 +46,7 @@ const DropdownComponent = ({ title, onScroll, onOpenChange, isOpen, children }) 
               className={
                 (onScroll > 200 ? "lg:text-primary-dark " : "lg:text-white ") +
                 oxygen +
-                "cursor-pointer flex items-end gap-2 transition-all hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-[21px]"
+                "cursor-pointer flex items-center gap-2 transition-all hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-lg"
               }
             >
               <p>{title}</p>
@@ -64,7 +68,7 @@ const MyNavbar = () => {
   const [isInitiativeOpen, setIsInitiativeOpen] = useState(false);
   const [isCommunityOpen, setIsCommunityOpen] = useState(false);
   const [isInformationOpen, setIsInformationOpen] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [scrollPos, setScrollPos] = useState(window.scrollY);
 
   useEffect (() => {
@@ -82,34 +86,41 @@ const MyNavbar = () => {
 
   return (
     <Navbar
-      // id="navbar"
+      onMenuOpenChange={setIsMenuOpen}
+      isMenuOpen={isMenuOpen}
       isBlurred={false}
       classNames={{
-        base: (scrollPos > 200 ? "lg:py-0 lg:bg-white " : "lg:py-4 lg:bg-transparent ") + "bg-white fixed py-4 border-b border-slate-200/50 transition-all shadow-lg drop-shadow-sm",
+        base: (scrollPos > 200 ? "lg:py-0 lg:bg-white " : "lg:py-4 lg:bg-transparent ") + "bg-white fixed py-0 border-b border-slate-200/50 transition-all shadow-lg drop-shadow-sm",
         wrapper: "max-w-[1300px]",
       }}
     >
-      <NavbarBrand>
-        <Link href="/">
-          <p
-            className={
-              (scrollPos > 200 ? "lg:text-primary-dark ":"lg:text-white " ) +
-              oxygen +
-              " __navbar-text__ text-[32px] font-bold transition-colors text-primary-dark"
-            }
-          >
-            ShoreLab
-          </p>
-        </Link>
-      </NavbarBrand>
-      <NavbarContent>
+      <NavbarContent justify="start">
+        <NavbarMenuToggle
+          icon={isMenuOpen ? <BsXLg size={30} />  : <BsList size={30} />}
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="w-fit aspect-square flex lg:hidden hover:bg-slate-200/50 text-teal-secondary h-min p-2"/>
+        <NavbarBrand>
+          <Link href="/">
+            <p
+              className={
+                (scrollPos > 200 ? "lg:text-primary-dark ":"lg:text-white " ) +
+                oxygen +
+                "text-3xl font-bold transition-colors text-primary-dark"
+              }
+            >
+              ShoreLab
+            </p>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden lg:flex gap-0" justify="end">
         <NavbarItem>
           <Link
             href="/"
             className={
               (scrollPos > 200 ? "lg:text-primary-dark ":"lg:text-white " ) +
               oxygen +
-              "transition-colors text-primary-dark hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-[21px]"
+              "transition-colors text-primary-dark hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-lg"
             }
           >
             Home
@@ -121,7 +132,7 @@ const MyNavbar = () => {
             className={
               (scrollPos > 200 ? "lg:text-primary-dark ":"lg:text-white " ) +
               oxygen +
-              "transition-colors text-primary-dark hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-[21px]"
+              "transition-colors text-primary-dark hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-lg"
             }
           >
             About Us
@@ -134,7 +145,7 @@ const MyNavbar = () => {
               className={
                 (scrollPos > 200 ? "lg:text-primary-dark " : "lg:text-white ") +
                 oxygen +
-                "cursor-pointer flex items-end gap-2 transition-all text-primary-dark hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-[21px]"
+                "cursor-pointer flex items-center gap-2 transition-all text-primary-dark hover:bg-teal-secondary/20 hover:!text-teal-secondary rounded-md px-3 py-1 text-lg"
               }
             >
               <p>Initiatives</p>
@@ -199,6 +210,8 @@ const MyNavbar = () => {
             ><span className="text-lg">Career</span></DropdownItem>
           </DropdownComponent>
         </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
         <NavbarItem>
           <Link href="/login">
             <LoginButton onScroll={scrollPos} size="sm">Login</LoginButton>
@@ -210,6 +223,64 @@ const MyNavbar = () => {
           </Link>
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu className={oxygen + "text-primary-dark p-0 gap-0 h-fit"}>
+        <NavbarMenuItem className="flex">
+          <Link onClick={() => setIsMenuOpen(false)} href="/" className="w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-4 border-b border-slate-200">
+            Home
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="flex">
+          <Link  onClick={()=>setIsMenuOpen(false)} href="/about" className="w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-4 border-b border-slate-200">
+            About Us
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="flex flex-col">
+          <button onClick={() => setIsInitiativeOpen(!isInitiativeOpen)} className="flex justify-between items-center w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-4 border-b border-slate-200">
+            <div>
+              Initiatives
+            </div>
+            <FiChevronDown size={25} className={(isInitiativeOpen ? "rotate-180 " : " ") + " transition-transform"} />
+          </button>
+          <Link onClick={()=>setIsMenuOpen(false)} href="/initiatives/shorelab-academy" className={(isInitiativeOpen ? "block ": "hidden ") + "w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-8 border-b border-slate-200"}>
+            ShoreLab Academy
+          </Link>
+          <Link onClick={()=>setIsMenuOpen(false)} href="/initiatives/projects" className={(isInitiativeOpen ? "block ": "hidden ") + "w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-8 border-b border-slate-200"}>
+            Other Projects
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="flex flex-col">
+          <button onClick={() => setIsCommunityOpen(!isCommunityOpen)} className="flex justify-between items-center w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-4 border-b border-slate-200">
+            <div>
+              Community
+            </div>
+            <FiChevronDown size={25} className={(isCommunityOpen ? "rotate-180 " : " ") + " transition-transform"} />
+          </button>
+          <Link onClick={()=>setIsMenuOpen(false)} href="#" className={(isCommunityOpen ? "block ": "hidden ") + "w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-8 border-b border-slate-200"}>
+            ShoreSnap
+          </Link>
+          <Link onClick={()=>setIsMenuOpen(false)} href="#" className={(isCommunityOpen ? "block ": "hidden ") + "w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-8 border-b border-slate-200"}>
+            ShoreLab Insight
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="flex flex-col">
+          <button onClick={() => setIsInformationOpen(!isInformationOpen)} className="flex justify-between items-center w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-4 border-b border-slate-200">
+            <div>
+              Information
+            </div>
+            <FiChevronDown size={25} className={(isInformationOpen ? "rotate-180 " : " ") + " transition-transform"} />
+          </button>
+          <Link onClick={()=>setIsMenuOpen(false)} href="#" className={(isInformationOpen ? "block ": "hidden ") + "w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-8 border-b border-slate-200"}>
+            Contact
+          </Link>
+          <Link onClick={()=>setIsMenuOpen(false)} href="#" className={(isInformationOpen ? "block ": "hidden ") + "w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-8 border-b border-slate-200"}>
+            Partnership
+          </Link>
+          <Link onClick={()=>setIsMenuOpen(false)} href="#" className={(isInformationOpen ? "block ": "hidden ") + "w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-8 border-b border-slate-200"}>
+            Career
+          </Link>
+        </NavbarMenuItem>
+        
+      </NavbarMenu>
     </Navbar>
   );
 };
